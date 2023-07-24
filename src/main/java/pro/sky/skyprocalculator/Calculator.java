9e5026d7051bc5cdced441d62e811671c3210cd6
Александр <alexst731w@gmail.com>
2023-07-24 19:23:48 +0300

    ДЗ

--- src/main/java/pro/sky/skyprocalculator/Calculator.java
+++ src/main/java/pro/sky/skyprocalculator/Calculator.java
@@ -4,19 +4,22 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class Calculator {
-    public int plus(int num1, int num2) {
-        return num1 + num2;
+    public String plus(int num1, int num2) {
+        int result = num1 + num2;
+        return num1 + " + " + num2 + " = " + result;
     }
-    public int minus(int num1, int num2) {
-        return num1 - num2;
+    public String minus(int num1, int num2) {
+        int result = num1 - num2;
+        return num1 + " - " + num2 + " = " + result;
     }
-    public int multiply(int num1, int num2) {
-        return num1 * num2;
+    public String multiply(int num1, int num2) {
+        int result = num1 * num2;
+        return num1 + " * " + num2 + " = " + result;
     }
-    public double divide(double num1, double num2) throws ArithmeticException {
-        String ArithmeticException;
+    public String divide(double num1, double num2) throws ArithmeticException {
+        double result = num1 / num2;
         if (num1 == 0 || num2 == 0) {
-        } throw new ArithmeticException("Деление на ноль");
-        return num1 / num2;
+            return "Ошибка, на 0 делить нельзя";
+        } else return num1 + " / " + num2 + " = " + result;
     }
 }
--- src/main/java/pro/sky/skyprocalculator/Controller.java
+++ src/main/java/pro/sky/skyprocalculator/Controller.java
@@ -20,19 +20,19 @@ public class Controller {
     }
 
     @GetMapping(path = "/calculator/plus")
-    public int plus(@RequestParam() int num1, int num2) {
+    public String plus(@RequestParam int num1,@RequestParam int num2) {
         return calculator.plus(num1, num2);
     }
     @GetMapping(path = "/calculator/minus")
-    public int minus(@RequestParam() int num1, int num2) {
+    public String minus(@RequestParam int num1,@RequestParam int num2) {
         return calculator.minus(num1, num2);
     }
     @GetMapping(path = "/calculator/multiply")
-    public int multiply(@RequestParam() int num1, int num2) {
+    public String multiply(@RequestParam int num1,@RequestParam int num2) {
         return calculator.multiply(num1, num2);
     }
     @GetMapping(path = "/calculator/divide")
-    public double divide(@RequestParam() double num1, double num2) {
+    public String divide(@RequestParam double num1,@RequestParam double num2) {
         return calculator.divide(num1, num2);
     }
 }
